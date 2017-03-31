c7bf48b5b6619f801038a7687fbbd0dad4be0285
Thomas Leroy <imtrip@gmail.com>
2017-03-31 13:20:11 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// require Express and create Express app
+var express = require('express');
+var app = express();
+// intialize url and request to use later
+var url = require('url');
+//var request = require('request');
+
+// require the body parser to parse json data and urls
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set the port to whaever or 9001
+app.set('port', (process.env.PORT || 9001));
+
+// for testing purpose on the root of app
+app.get('/', function(req, res){
+  res.send('Get request works!');
+});
+
+app.post('/post', function(req, res){
+  console.log(req.body.text);
+  res.send('Response to POST request')
+  });
+
+/*
+app.post('/post', function(req, res){
+  var parsed_url = url.format({
+    pathname: 'https://api.genius.com/search',
+    query: {
+      access_token: process.env.GENIUS_ACCESS,
+      q: req.body.text
+    }
+  });
+
+  request(parsed_url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      var first_url = data.response.hits[0].result.url;
+
+      var body = {
+        response_type: "in_channel",
+        text: first_url
+      };
+
+      res.send(body);
+    }
+  });
+});*/
+
+// Listen!
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
