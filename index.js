59f5c9cc244aea8d19add06d362dd237a817e367
Thomas Leroy <imtrip@gmail.com>
2017-03-31 18:20:06 +0200

    First bot

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 // require Express and create Express app
 var express = require('express');
 var app = express();
-// intialize url and request to use later
-var url = require('url');
-//var request = require('request');
 
 // require the body parser to parse json data and urls
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// set the port to whaever or 9001
-app.set('port', (process.env.PORT || 9001));
 
 // for testing purpose on the root of app
 app.get('/', function(req, res){
@@ -19,40 +13,35 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-  console.log(req.body.text);
+  // get the command text
+  var text = req.body.text;
+  // if we get multiple arguments at the command, we'll need an array:
+  // the text is returned as a string only, we'll create an array of strings
+/*
+  if (!text === ''){
+    text = text.split(' ');
+    // Now if we get more than one words after the command
+    if (text.length > 1) {
+      // make a call for a search in the subreddit, for example
+    }
+    // else, we only got 1 word after the command, that's the subreddit only
+    else (text.length === 1) {
+      // post a random post from the subreddit
+    }
+  }
+*/
+
+    console.log(text);
+
   var data = {
     response_type: "ephemeral",
-    text: 'Reddit Bot Response to POST'
-  }
-  res.send(data.text);
-  });
-
-/*
-app.post('/post', function(req, res){
-  var parsed_url = url.format({
-    pathname: 'https://api.genius.com/search',
-    query: {
-      access_token: process.env.GENIUS_ACCESS,
-      q: req.body.text
+    text: "*RESPONSE TITLE*",
+    attachments:[
+    {
+      text: 'Reddit Bot Speaks!'
     }
+    ]};
+  res.send(data);
   });
 
-  request(parsed_url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      var first_url = data.response.hits[0].result.url;
-
-      var body = {
-        response_type: "in_channel",
-        text: first_url
-      };
-
-      res.send(body);
-    }
-  });
-});*/
-
-// Listen!
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(9001);
